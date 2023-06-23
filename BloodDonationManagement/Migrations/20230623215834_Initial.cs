5ed01b28e98132c469b166c1376af5c7faf741d2
JuliaCMS <julia.cms@outlook.com>
2023-06-23 19:19:11 -0300

    Applied Initial Migration to the Database

---
+++ BloodDonationManagement/Migrations/20230623215834_Initial.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BloodDonationManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Cep = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Street = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Number = table.Column<int>(type: "int", nullable: true),
+                    Neighborhood = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    State = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Admins",
+                columns: table => new
+                {
+                    Login = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admins", x => x.Login);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BloodTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ABO = table.Column<int>(type: "int", nullable: false),
+                    Rh = table.Column<int>(type: "int", nullable: false),
+                    Component = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BloodTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BloodBanks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AdressId = table.Column<int>(type: "int", nullable: true),
+                    Telephone = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BloodBanks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BloodBanks_Addresses_AdressId",
+                        column: x => x.AdressId,
+                        principalTable: "Addresses",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Donors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Cpf = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AdressId = table.Column<int>(type: "int", nullable: true),
+                    Telephone1 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Telephone2 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BloodTypeId = table.Column<int>(type: "int", nullable: true),
+                    LastDonation = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Donors", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Donors_Addresses_AdressId",
+                        column: x => x.AdressId,
+                        principalTable: "Addresses",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Donors_BloodTypes_BloodTypeId",
+                        column: x => x.BloodTypeId,
+                        principalTable: "BloodTypes",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BloodInventories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BloodTypeId = table.Column<int>(type: "int", nullable: true),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    FkBloodBank = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BloodInventories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BloodInventories_BloodBanks_FkBloodBank",
+                        column: x => x.FkBloodBank,
+                        principalTable: "BloodBanks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BloodInventories_BloodTypes_BloodTypeId",
+                        column: x => x.BloodTypeId,
+                        principalTable: "BloodTypes",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Requisitions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RequisitionDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FkDonor = table.Column<int>(type: "int", nullable: false),
+                    FkBloodBank = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Requisitions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Requisitions_BloodBanks_FkBloodBank",
+                        column: x => x.FkBloodBank,
+                        principalTable: "BloodBanks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Requisitions_Donors_FkDonor",
+                        column: x => x.FkDonor,
+                        principalTable: "Donors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BloodBanks_AdressId",
+                table: "BloodBanks",
+                column: "AdressId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BloodInventories_BloodTypeId",
+                table: "BloodInventories",
+                column: "BloodTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BloodInventories_FkBloodBank",
+                table: "BloodInventories",
+                column: "FkBloodBank",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Donors_AdressId",
+                table: "Donors",
+                column: "AdressId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Donors_BloodTypeId",
+                table: "Donors",
+                column: "BloodTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Requisitions_FkBloodBank",
+                table: "Requisitions",
+                column: "FkBloodBank");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Requisitions_FkDonor",
+                table: "Requisitions",
+                column: "FkDonor");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admins");
+
+            migrationBuilder.DropTable(
+                name: "BloodInventories");
+
+            migrationBuilder.DropTable(
+                name: "Requisitions");
+
+            migrationBuilder.DropTable(
+                name: "BloodBanks");
+
+            migrationBuilder.DropTable(
+                name: "Donors");
+
+            migrationBuilder.DropTable(
+                name: "Addresses");
+
+            migrationBuilder.DropTable(
+                name: "BloodTypes");
+        }
+    }
+}
